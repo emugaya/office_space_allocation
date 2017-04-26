bcfc21c867c1d55b478e6c956ad7446018477798
Ezekiel Mugaya <emugaya@gmail.com>
2017-04-27 00:06:50 +0300

    Added attribute proom_name which is being used to manage reallocations. It stores the current room a user is allocated

--- dojo/models/person.py
+++ dojo/models/person.py
@@ -3,18 +3,22 @@ class Person(object):
         self.person_id = person_id
         self.last_name = last_name
         self.first_name =first_name
+        self.room_name = ''
 
 class Fellow(Person):
-    def __init__(self, person_id, last_name, first_name,person_type, wants_accommodation =None):
+    def __init__(self, person_id, last_name, first_name,person_type, wants_accommodation =None, proom_name=''):
         self.person_id = person_id
         self.last_name = last_name
         self.first_name = first_name
         self.person_type = person_type
         self.wants_accommodation = wants_accommodation
+        self.proom_name = proom_name
+
 
 class Staff(Person):
-    def __init__(self,person_id, last_name, first_name,person_type):
+    def __init__(self,person_id, last_name, first_name, person_type, proom_name=''):
         self.person_id = person_id
         self.last_name = last_name
         self.first_name = first_name
         self.person_type = person_type
+        self.room_name =''
