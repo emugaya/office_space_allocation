600a54e8bf861a83bc163f7a60f13644d26be3e1
Ezekiel Mugaya <emugaya@gmail.com>
2017-06-22 15:14:10 +0300

    Implementation of print_allocations test cases completed

--- dojo/tests/test_print_allocations.py
+++ dojo/tests/test_print_allocations.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from dojo.models.dojo import Dojo
 
@@ -6,7 +7,45 @@ class TestPrintAllocations(unittest.TestCase):
     This Object Tests Fuctionality of the print_alllocations
     """
     def test_print_allocations_succesfully(self):
-        pass
+        """
+        This case tests whether print_allocations succesfully returns rooms
+        and there members to be printed on the screen or file.
+        """
+        #create new Dojo Object.
+        my_dojo = Dojo()
+        #create an office room in the dojo
+        my_dojo.create_room('office', ['Blue'])
+        #create a living room in the dojo
+        my_dojo.create_room('livingspace', ['Pink'])
+        # Add 6 people(4 staff and 2 fellows) to dojo from text file by calling load_people
+        my_dojo.load_people('load_people_data')
+        self.assertEqual(my_dojo.print_allocations,
+                               {'blue': ['Mugaya Ezekiel',
+                                          'Roza Kobel',
+                                          'Favour Kwagala',
+                                          'Joyce Mummy',
+                                          'Isiaha Katumwa',
+                                          'Paul Kobel'],\
+                                'pink': ['Roza Kobel','Paul Kobel']
+                                },
+                                msg = "Print Allocations doesn't return values to be printed to screen"
+                                )
 
     def test_print_allocations_prints_to_file(self):
-        pass
+        """
+        This case tests whether print_allocations prints to a file if the file name is supplied.
+        """
+        my_dojo = Dojo()
+        #create an office room in the dojo
+        my_dojo.create_room('office', ['Blue'])
+        #create a living room in the dojo
+        my_dojo.create_room('livingspace', ['Pink'])
+        # Add 6 people(4 staff and 2 fellows) to dojo from text file by calling load_people
+        my_dojo.load_people('load_people_data')
+
+        self.assertEqual(my_dojo.print_allocations('test_print_allocations_file'),
+                                                   "Allocations Have been \
+                                                   printed to file succesfully",
+                                                   msg="Allocations not succesfully printed to file"
+                                                   )
+        self.assertEqual(os.path.exists('test_print_allocations_file.txt'),True , msg="File not created")
